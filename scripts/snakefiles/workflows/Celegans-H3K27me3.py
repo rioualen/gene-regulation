3e1434c9942a2c7899b135f1a4be99d8f4aa68cf
Claire Rioualen <claire.rioualen@inserm.fr>
2015-07-31 12:18:43 +0200

    Celegans pipeline update

--- scripts/snakefiles/workflows/Celegans-H3K27me3.py
+++ scripts/snakefiles/workflows/Celegans-H3K27me3.py
@@ -49,9 +49,9 @@ SAMPLES = CHIP + INPUT
 GENOME = config["genome"]["genome_version"]
 
 # /!\ lists vs var
-TRIMMING = "sickle_se_q" + config['sickle']['threshold']
-ALIGNERS = "bwa".split()
-
+TRIMMING = "sickle-se-q" + config['sickle']['threshold']
+ALIGNER = "bwa"
+PEAK_CALLER = "macs2"
 
 #================================================================#
 #                         Workflow                               #
@@ -78,17 +78,19 @@ BWA_INDEX = expand(config["dir"]["genome"] + "BWAIndex/{genome}/{genome}.fa.bwt"
 BWA_MAPPING = expand(RESULTSDIR + "{samples}/{samples}_" + TRIMMING + "_bwa.sam", samples=SAMPLES)
 
 # File conversion
-SAM_TO_BAM = expand(RESULTSDIR + "{sample}/{sample}_" + TRIMMING + "_{aligner}.bam", sample=SAMPLES, aligner=ALIGNERS)
-BAM_TO_BED = expand(RESULTSDIR + "{sample}/{sample}_" + TRIMMING + "_{aligner}.bed", sample=SAMPLES, aligner=ALIGNERS)
+SAM_TO_BAM = expand(RESULTSDIR + "{sample}/{sample}_" + TRIMMING + "_{aligner}.bam", sample=SAMPLES, aligner=ALIGNER)
+BAM_TO_BED = expand(RESULTSDIR + "{sample}/{sample}_" + TRIMMING + "_{aligner}.bed", sample=SAMPLES, aligner=ALIGNER)
+CONVERTED_BED = expand(RESULTSDIR + "{sample}/{sample}_" + TRIMMING + "_{aligner}.converted.bed", sample=SAMPLES, aligner=ALIGNER)
 
 # Peak-calling
-#HOMER = expand(RESULTSDIR + "{chip}_vs_{inp}/{chip}_vs_{inp}_" + TRIMMING + "_{aligner}_homer.bed", chip=CHIP, inp=INPUT, aligner=ALIGNERS)
-#HOMER = expand(RESULTSDIR + "GSM1217459_vs_GSM1217457/GSM1217459_vs_GSM1217457_homer.bed")
+# ! In case of use of several peak-callers, beware of specific prefixes or such...
+#MACS2 = expand(RESULTSDIR + "{chip}_vs_{inp}/{chip}_vs_{inp}_{trimming}_{aligner}_{caller}_peaks.narrowPeak", chip="GSM121459", inp="GSM1217457", trimming=TRIMMING, aligner=ALIGNER, caller=PEAK_CALLER)
+#MACS2 = expand(RESULTSDIR + "{chip}_vs_{inp}/{chip}_vs_{inp}_{trimming}_{aligner}_{caller}._summits.bed", chip=CHIP, inp=INPUT, trimming=TRIMMING, aligner=ALIGNER, caller=PEAK_CALLER)
 
-MACS2 = expand(RESULTSDIR + "{chip}_vs_{inp}/{chip}_vs_{inp}_" + TRIMMING + "_{aligner}_summits.bed", chip=CHIP, inp=INPUT, aligner=ALIGNERS)
-
-## File conversion
-#BED_TO_FASTA = expand(RESULTSDIR + '{sample}.fasta', sample=SAMPLES)
+# File conversion
+#NARROWPEAK_TO_BED = expand(RESULTSDIR + "{chip}_vs_{inp}/{chip}_vs_{inp}_{trimming}_{aligner}_{caller}_peaks.bed", chip=CHIP, inp=INPUT, trimming=TRIMMING, aligner=ALIGNER, caller=PEAK_CALLER)
+#BED_TO_FASTA = expand(RESULTSDIR + "{chip}_vs_{inp}/{chip}_vs_{inp}_{trimming}_{aligner}_{caller}_peaks.fasta", chip=CHIP, inp=INPUT, trimming=TRIMMING, aligner=ALIGNER, caller=PEAK_CALLER)
+BED_TO_FASTA = expand(RESULTSDIR + "{sample}/{sample}_" + TRIMMING + "_{aligner}.calling.fasta", sample=SAMPLES, trimming=TRIMMING, aligner=ALIGNER)
 
 ## Sequence purge
 #PURGED_SEQ = expand(RESULTSDIR + "{sample}_purged.fasta", sample=SAMPLES)
@@ -101,22 +103,29 @@ MACS2 = expand(RESULTSDIR + "{chip}_vs_{inp}/{chip}_vs_{inp}_" + TRIMMING + "_{a
 ## Peaks length
 #PEAK_LENGTH = expand(RESULTSDIR + '{sample}_purged_length.png', sample=SAMPLES)
 
+#ruleorder: macs2 > bam_to_bed
+
+CLEANING = expand(RESULTSDIR + "cleaning.done")
 
 rule all: 
     """
     Run all the required analyses
     """
-    input: GRAPHICS, RAW_QC, TRIMMED_QC, BWA_MAPPING
+    input: GRAPHICS, RAW_QC, TRIMMED_QC, BAM_TO_BED
     params: qsub=config["qsub"]
     shell: "echo Job done    `date '+%Y-%m-%d %H:%M'`"
 
-
+#, RAW_QC, TRIMMED_QC, CLEANING
 
 # GRAPHICS -> all
 # RAW_QC ?
 # IMPORT -> SICKLE_TRIMMING -> TRIMMED_QC -> all
 # BWA_INDEX -> BWA_MAPPING, BOWTIE_MAPPING -> SAM_TO_BAM -> BAM_TO_BED -> all
 # BED_TO_FASTA -> PURGED_SEQUENCE -> (OLIGO_ANALYSIS, PEAK_LENGTH, PEAK_MOTIFS) -> all
+
+#================================================================#
+#                       Local rules                              #
+#================================================================#
 
 rule raw_data:
 	"""
@@ -129,16 +138,36 @@ rule raw_data:
 	params: sample = "{samples}"
 	shell: "var=$(ls {input}); fastq-dump --outdir {output.dir} {input}$var; srr=$(basename $var .sra); mv {output.dir}$srr.fastq {output.dir}{params.sample}.fastq"
 
+
+rule bed_chr_id_conversion:
+	input: "{file}.bed"
+	output:	"{file}.converted.bed"
+	shell:"awk '{{gsub(\"NC_003279.8\",\"chrI\"); gsub(\"NC_003280.10\",\"chrII\"); gsub(\"NC_003281.10\",\"chrIII\"); gsub(\"NC_003282.8\",\"chrIV\"); gsub(\"NC_003283.11\",\"chrV\"); gsub(\"NC_003284.9\",\"chrX\"); print}}' {input} > {output[0]}"
+
+#		out = open(output[0], "w")
+#		with open(input.bed) as bedfile:
+#			for line1 in bedfile:
+#				RefSeq_id_1 = line1.split()[0]
+#				other_info = '\t'.join(line1.split()[1:len(line1.split())])
+#				with open(input.table) as conversion_table:
+#					for line2 in conversion_table:
+#						RefSeq_id_2 = line2.split()[0]
+#						chr_id = line2.split()[1]
+#						if RefSeq_id_1 == RefSeq_id_2:
+#							towrite = chr_id + '\t' + other_info + '\n'
+#							out.write(towrite)
+
 #================================================================#
 #                         Includes                               #
 #================================================================#
 
-#include: RULES + "bed_to_fasta.rules"
+include: RULES + "bed_to_fasta.rules"
 #include: RULES + "bowtie.rules"
 include: RULES + "bwa_index.rules"
 include: RULES + "bwa_se.rules"
-#include: RULES + "convert_bam_to_bed.rules"
-#include: RULES + "convert_sam_to_bam.rules"
+include: RULES + "clean.rules"
+include: RULES + "convert_bam_to_bed.rules"
+include: RULES + "convert_sam_to_bam.rules"
 #include: RULES + "count_oligo.rules"
 include: RULES + "fastqc.rules"
 include: RULES + "flowcharts.rules"
