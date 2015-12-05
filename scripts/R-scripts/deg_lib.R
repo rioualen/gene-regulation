9cd971cf39dd08ca3614904fa5b409dcb574c817
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2015-12-05 11:01:02 +0100

    added some sample-wise stats

--- scripts/R-scripts/deg_lib.R
+++ scripts/R-scripts/deg_lib.R
@@ -522,20 +522,23 @@ calc.stats.per.sample <- function(sample.descriptions,
   stats.per.sample <- cbind(  
     sample.desc[names(all.counts), ],
     data.frame(
-      "sum" = apply(all.counts, 2, sum, na.rm=TRUE),
-      "mean" = apply(all.counts, 2, mean, na.rm=TRUE),
-      "min" = apply(all.counts, 2, min, na.rm=TRUE),
-      "perc05" = apply(all.counts, 2, quantile, probs=0.05, na.rm=TRUE),
-      "perc25" = apply(all.counts, 2, quantile, probs=0.25, na.rm=TRUE),
-      "median" = apply(all.counts, 2, median, na.rm=TRUE),
-      "perc75" = apply(all.counts, 2, quantile, probs=0.75, na.rm=TRUE),
-      "perc95" = apply(all.counts, 2, quantile, probs=0.95, na.rm=TRUE),
-      "max" = apply(all.counts, 2, max, na.rm=TRUE),
-      "zeros" = apply(all.counts == 0, 2, sum, na.rm=TRUE)
+      "zeros" = apply(all.counts == 0, 2, sum, na.rm=TRUE), ## Number of genes with 0 counts
+      "detected" = apply(all.counts > 0, 2, sum, na.rm=TRUE), ## Number of genes counted at least once
+      "sum" = apply(all.counts, 2, sum, na.rm=TRUE), ## Sum of all counts for the sample
+      "mean" = apply(all.counts, 2, mean, na.rm=TRUE), ## Mean counts per gene
+      "min" = apply(all.counts, 2, min, na.rm=TRUE), ## Min counts per gene 
+      "perc05" = apply(all.counts, 2, quantile, probs=0.05, na.rm=TRUE), ## 5th percentile
+      "perc25" = apply(all.counts, 2, quantile, probs=0.25, na.rm=TRUE), ## 25th percentile
+      "median" = apply(all.counts, 2, median, na.rm=TRUE), ## median (percentile 50)
+      "perc75" = apply(all.counts, 2, quantile, probs=0.75, na.rm=TRUE), ## percentile 75
+      "perc95" = apply(all.counts, 2, quantile, probs=0.95, na.rm=TRUE), ## percentile 95
+      "max" = apply(all.counts, 2, max, na.rm=TRUE) ## Max counts per gene
     )
   )
+  stats.per.sample$max.sum.ratio <- stats.per.sample$max / stats.per.sample$sum
+  stats.per.sample$median.mean.ratio <- stats.per.sample$media / stats.per.sample$mean
   stats.per.sample$Mreads <- round(stats.per.sample$sum/1e6, digits = 1)
-  
+
   ## Count number and the fraction of samples with counts below the mean. 
   ## This shows the impact of very large counts: in some samples, 
   ## 85% of the samples have a value below the mean (i.e. the mean is at the percentile 85 !)
