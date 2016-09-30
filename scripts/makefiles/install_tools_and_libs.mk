a1eb835936482fab75bfaebd5acddd20308edd86
rioualen <claire.rioualen@inserm.fr>
2016-09-30 12:48:53 +0200

    minor

--- scripts/makefiles/install_tools_and_libs.mk
+++ scripts/makefiles/install_tools_and_libs.mk
@@ -276,8 +276,8 @@ bwa:
 subread:
 	cd $(SOURCE_DIR); \
 	wget https://sourceforge.net/projects/subread/files/subread-$(SUBREAD_VER)/subread-$(SUBREAD_VER)-source.tar.gz; \
-#	tar zxvf subread-$(SUBREAD_VER)-source.tar.gz; \
-#	cd subread-$(SUBREAD_VER)-source/src; \
+	tar zxvf subread-$(SUBREAD_VER)-source.tar.gz; \
+	cd subread-$(SUBREAD_VER)-source/src; \
 #	make -f Makefile.Linux; \
 #	cd ../bin; \
 #	cp `find * -executable -type f` $(BIN_DIR);\
