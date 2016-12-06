4c3792020b97fc4266d020bc50e45492aeaa8845
rioualen <claire.rioualen@inserm.fr>
2016-12-06 10:32:22 +0100

    some cleaning

--- scripts/snakefiles/workflows/ChIP-seq_workflow_SE.py
+++ scripts/snakefiles/workflows/ChIP-seq_workflow_SE.py
@@ -4,9 +4,10 @@ of transcription factors.
 
 This workflow performs the following treatments: 
 
+ - quality check
  - read mapping
  - peak-calling with alternate peak-calling programs
-( - motif discovery)
+ - motif discovery
 
 The details are specified in a yaml-formatted configuration file.
 
@@ -53,32 +54,21 @@ import pandas as pd
 
 GENEREG_LIB = os.path.join(config["dir"]["base"], config["dir"]["gene_regulation"])
 
-# Python includes
-PYTHON = os.path.join(GENEREG_LIB, "scripts/python_lib")
-include: os.path.join(PYTHON, "util.py")
+include: os.path.join(GENEREG_LIB, "scripts/python_lib/util.py")
 
 
 #================================================================#
-#                      Global variables
+#                      Variables
 #================================================================#
-
-# Raw data
-if not (("dir" in config.keys()) and ("reads_source" in config["dir"].keys())):
-    sys.exit("The parameter config['dir']['reads_source'] should be specified in the config file.")
-else:
-    READS = os.path.join(config["dir"]["base"], config["dir"]["reads_source"])
-
-# Samples
-#SAMPLES = read_table(config["metadata"]["samples"])
-#SAMPLE_IDS = SAMPLES.iloc[:,0]
-
-SAMPLE_IDS = read_table(os.path.join(config["dir"]["base"], config["metadata"]["samples"]))['ID']
 
 # Genome & annotations
 GENOME_DIR = os.path.join(config["dir"]["base"], config["dir"]["genome"])
 GENOME_FASTA = os.path.join(GENOME_DIR, config["genome"]["fasta_file"])
 GENOME_GFF3 = os.path.join(GENOME_DIR, config["genome"]["gff3_file"])
 GENOME_GTF = os.path.join(GENOME_DIR, config["genome"]["gtf_file"])
+
+# Samples
+SAMPLE_IDS = read_table(os.path.join(config["dir"]["base"], config["metadata"]["samples"]))['ID']
 
 # Design
 DESIGN = read_table(os.path.join(config["dir"]["base"],config["metadata"]["design"]))
@@ -88,8 +78,8 @@ CONTROL = DESIGN['control']
 ## Data & results dir
 if not (("dir" in config.keys()) and ("reads_source" in config["dir"].keys())):
     sys.exit("The parameter config['dir']['reads_source'] should be specified in the config file.")
-
-READS = os.path.join(config["dir"]["base"],config["dir"]["reads_source"])
+else:
+    READS = os.path.join(config["dir"]["base"], config["dir"]["reads_source"])
 
 if not ("fastq" in config["dir"].keys()):
     sys.exit("The parameter config['dir']['fastq'] should be specified in the config file.")
@@ -155,10 +145,11 @@ include: os.path.join(RULES, "macs14.rules")
 include: os.path.join(RULES, "peak_motifs.rules")
 include: os.path.join(RULES, "sickle.rules")
 include: os.path.join(RULES, "spp.rules")
+include: os.path.join(RULES, "sra_to_fastq.rules")
 include: os.path.join(RULES, "subread_index.rules")
 include: os.path.join(RULES, "subread_align.rules")
 include: os.path.join(RULES, "swembl.rules")
-include: os.path.join(RULES, "sra_to_fastq.rules")
+
 
 #================================================================#
 #                         Workflow                               #
@@ -170,10 +161,7 @@ IMPORT = expand(FASTQ_DIR + "/{samples}/{samples}.fastq", samples=SAMPLE_IDS)
 ## Graphics & reports
 GRAPHICS = expand(REPORTS_DIR + "/{graph}.png", graph=["dag", "rulegraph"])
 
-#----------------------------------------------------------------#
-# Quality control
-#----------------------------------------------------------------#
-
+## Quality report
 RAW_QC = expand(FASTQ_DIR + "/{samples}/{samples}_fastqc/{samples}_fastqc.html", samples=SAMPLE_IDS)
 QC = RAW_QC
 
@@ -186,11 +174,9 @@ if not (("tools" in config.keys()) and ("trimming" in config["tools"].keys())):
 
 TRIMMING_TOOLS = config["tools"]["trimming"].split()
 
-TRIMMING = expand(FASTQ_DIR + "/{samples}/{samples}_{trimmer}", samples=SAMPLE_IDS, trimmer=TRIMMING_TOOLS)
-TRIM = expand("{trimming}.fastq", trimming=TRIMMING)
+TRIMMING = expand(FASTQ_DIR + "/{samples}/{samples}_{trimmer}.fastq", samples=SAMPLE_IDS, trimmer=TRIMMING_TOOLS)
 
 TRIM_QC = expand(FASTQ_DIR + "/{samples}/{samples}_{trimmer}_fastqc/{samples}_{trimmer}_fastqc.html", samples=SAMPLE_IDS, trimmer=TRIMMING_TOOLS)
-
 QC = RAW_QC + TRIM_QC
 
 #----------------------------------------------------------------#
@@ -209,15 +195,17 @@ if TRIMMING_TOOLS:
 else:
     PREFIX = expand("{aligner}", aligner=MAPPING_TOOLS)
 
-ALIGNMENT=expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}", samples=SAMPLE_IDS, prefix=PREFIX)
+MAPPING         = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}.bam", samples=SAMPLE_IDS, prefix=PREFIX)
 
-MAPPING = expand("{alignment}.bam", alignment=ALIGNMENT)
+SORTED_BAM      = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}_sorted_pos.bam", samples=SAMPLE_IDS, prefix=PREFIX)
+SORTED_BAM_BAI  = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}_sorted_pos.bam.bai", samples=SAMPLE_IDS, prefix=PREFIX)
+BAM_STATS       = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}_bam_stats.txt", samples=SAMPLE_IDS, prefix=PREFIX)
+SORTED_BED      = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}_sorted_pos.bed", samples=SAMPLE_IDS, prefix=PREFIX)
 
-SORTED_BAM = expand("{alignment}_sorted_pos.bam", alignment=ALIGNMENT)
-SORTED_BAM_BAI = expand("{alignment}_sorted_pos.bam.bai", alignment=ALIGNMENT)
-BAM_STATS = expand("{alignment}_bam_stats.txt", alignment=ALIGNMENT)
-
-SORTED_BED = expand("{alignment}_sorted_pos.bed", alignment=ALIGNMENT)
+GENOME_COV      = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}.bedgraph", samples=SAMPLE_IDS, prefix=PREFIX)
+GENOME_COV_GZ   = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}.bedgraph.gz", samples=SAMPLE_IDS, prefix=PREFIX)
+GENOME_COV_TDF  = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}.tdf", samples=SAMPLE_IDS, prefix=PREFIX)
+GENOME_COV_BW   = expand(SAMPLE_DIR + "/{samples}/{samples}_{prefix}.bw", samples=SAMPLE_IDS, prefix=PREFIX)
 
 # ----------------------------------------------------------------
 # Peak-calling
@@ -227,40 +215,28 @@ if not (("tools" in config.keys()) and ("peakcalling" in config["tools"].keys())
     sys.exit("The parameter config['tools']['peakcalling'] should be specified in the config file.")
 
 PEAKCALLING_TOOLS = config["tools"]["peakcalling"].split()
-PEAKCALLING=expand(expand(PEAKS_DIR + "/{treat}_vs_{control}/{{peakcaller}}/{treat}_vs_{control}_{{trimmer}}_{{aligner}}_{{peakcaller}}", zip, treat=TREATMENT, control=CONTROL), peakcaller=PEAKCALLING_TOOLS, aligner=MAPPING_TOOLS, trimmer=TRIMMING_TOOLS)
-
-PEAKS = expand("{peakcalling}.bed", peakcalling=PEAKCALLING)
+PEAKS       = expand(expand(PEAKS_DIR + "/{treat}_vs_{control}/{{peakcaller}}/{treat}_vs_{control}_{{trimmer}}_{{aligner}}_{{peakcaller}}.bed", zip, treat=TREATMENT, control=CONTROL), peakcaller=PEAKCALLING_TOOLS, aligner=MAPPING_TOOLS, trimmer=TRIMMING_TOOLS)
+GET_FASTA   = expand(expand(PEAKS_DIR + "/{treat}_vs_{control}/{{peakcaller}}/{treat}_vs_{control}_{{trimmer}}_{{aligner}}_{{peakcaller}}.fasta", zip, treat=TREATMENT, control=CONTROL), peakcaller=PEAKCALLING_TOOLS, aligner=MAPPING_TOOLS, trimmer=TRIMMING_TOOLS)
 
 # ----------------------------------------------------------------
 # Peak annotation
 # ----------------------------------------------------------------
 
-DISTANCE = ["intersect"]#, "window"]  #, "window"]#"closest" ## todo put in the config file
-GENOMIC_FEAT = expand("{peakcalling}_{distance}_annot.bed", peakcalling=PEAKCALLING, distance=DISTANCE)
-GENE_LIST = expand("{peakcalling}_{distance}_annot_gene_list.tab", peakcalling=PEAKCALLING, distance=DISTANCE)
+if not (("tools" in config.keys()) and ("annotation" in config["tools"].keys())):
+    sys.exit("The parameter config['tools']['annotation'] should be specified in the config file.")
 
-MOTIFS=expand(expand("{treat}_vs_{control}/{{peakcaller}}/peak-motifs/{treat}_vs_{control}_{{trimmer}}_{{aligner}}_{{peakcaller}}_peak-motifs_synthesis", zip, treat=TREATMENT, control=CONTROL), peakcaller=PEAKCALLING_TOOLS, aligner=MAPPING_TOOLS, trimmer=TRIMMING_TOOLS)
+DISTANCE = config["tools"]["annotation"].split()
+GENOMIC_FEAT    = expand(expand(PEAKS_DIR + "/{treat}_vs_{control}/{{peakcaller}}/{treat}_vs_{control}_{{peakcaller}}_{{distance}}_annot.bed", zip, treat=TREATMENT, control=CONTROL), peakcaller=PEAKCALLING_TOOLS, distance=DISTANCE)
+GENE_LIST       = expand(expand(PEAKS_DIR + "/{treat}_vs_{control}/{{peakcaller}}/{treat}_vs_{control}_{{peakcaller}}_{{distance}}_annot_gene_list.tab", zip, treat=TREATMENT, control=CONTROL), peakcaller=PEAKCALLING_TOOLS, distance=DISTANCE)
 
-GET_FASTA = expand(PEAKS_DIR + "/{peakcalling}.fasta", peakcalling=PEAKCALLING)
-PEAK_MOTIFS = expand(PEAKS_DIR + "/{motifs}.html", motifs=MOTIFS)
+PEAK_MOTIFS     = expand(expand(PEAKS_DIR + "/{treat}_vs_{control}/{{peakcaller}}/peak-motifs/{treat}_vs_{control}_{{trimmer}}_{{aligner}}_{{peakcaller}}_peak-motifs_synthesis.html", zip, treat=TREATMENT, control=CONTROL), peakcaller=PEAKCALLING_TOOLS, aligner=MAPPING_TOOLS, trimmer=TRIMMING_TOOLS)
 
 # ----------------------------------------------------------------
 # Visualization & reports
 # ----------------------------------------------------------------
 
-GENOME_INDEX = GENOME_FASTA + ".fai"
-GENOME_COVERAGE = expand("{alignment}.bedgraph", alignment=ALIGNMENT)
-GENOME_COVERAGE_GZ = expand("{alignment}.bedgraph.gz", alignment=ALIGNMENT)
-GENOME_COVERAGE_TDF = expand("{alignment}.tdf", alignment=ALIGNMENT)
-GENOME_COVERAGE_BIGWIG = expand("{alignment}.bw", alignment=ALIGNMENT)
-
-#GENOME_COVERAGE_BIGWIG_NORMALIZED = expand(expand(PEAKS_DIR + "/{treat}_vs_{control}/{{peakcaller}}/{treat}_vs_{control}_{{trimmer}}_{{aligner}}.bw", zip, treat=TREATMENT, control=CONTROL), aligner=MAPPING_TOOLS, trimmer=TRIMMING_TOOLS)
-
-
 ## Following not yet properly implemented
 IGV = expand(REPORTS_DIR + "/igv_session.xml")
-#BED_INTER = expand(REPORTS_DIR + "multiinter.tab")
-
 
 #================================================================#
 #                        Rule all                                #
@@ -273,15 +249,15 @@ rule all:
 	input: \
 #            IMPORT, \
             QC, \
-#            TRIM, \
+#            TRIMMING, \
 #            INDEX, \
 #            MAPPING, \
 #            SORTED_BAM, \
             BAM_STATS, \
 #            SORTED_BAM_BAI, \
             GENOME_INDEX, \
-            GENOME_COVERAGE_GZ, \
-            GENOME_COVERAGE_BIGWIG, \
+            GENOME_COV_GZ, \
+            GENOME_COV_BW, \
 #            SORTED_BED, \
 #            PEAKS, \
 #            GENOMIC_FEAT, \
