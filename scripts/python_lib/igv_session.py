002e938e9703e238d923cb9e73e788e27da9466b
rioualen <claire.rioualen@inserm.fr>
2017-04-25 15:39:32 +0200

    modified paths in xml file

--- scripts/python_lib/igv_session.py
+++ scripts/python_lib/igv_session.py
@@ -3,7 +3,7 @@
 
 
 ## TODO 
-#- chose output location
+#- chose output location and adapt paths to workdir //!\\
 #- check params
 #    - mandatory: filename, genome
 #    - optional: all other fields (peaks, coverage files, annotation files, etc)
@@ -15,7 +15,7 @@
 #    - etc
 #- add fields:
 #    - transcripts gtf from cufflinks
-#    - bam files (! can be very slow to load, rely on bam.bai files)
+#    - bam files (! can be very slow to load, relies on bam.bai files)
 #    - additional annotation files (like RegulonDB sites or other files mentionned in the config.yml)
 #- by default the session will display *all* peakfiles, *all* coverage files, etc. 
 #  Later on I need to think of a clean way for the user to customize what to display (only peaks from homer, only bam files from bowtie, whatever)
@@ -36,69 +36,68 @@ def igv_session(filename, genome, gtf="", gff3="", peaks="", coverage="", transc
     file.write('<Session genome="' + cwd + "/" + genome + '" hasGeneTrack="true" hasSequenceTrack="true" locus="" path="' + filename + '" version="8">\n')
 
     ## Resource files
-    file.write('    <Resources>\n')
+    file.write('<Resources>\n')
     if peaks:
         for i in peaks:
-            file.write('<Resource path="' + i + '"/>\n')
+            file.write('  <Resource path="../../' + i + '"/>\n')
     if coverage:
         for i in coverage:
-            file.write('<Resource path="' + i + '"/>\n')
+            file.write('  <Resource path="../../' + i + '"/>\n')
     if gtf:
-        file.write('<Resource path="' + gtf + '"/>\n')
+        file.write('  <Resource path="../../' + gtf + '"/>\n')
     if gff3:
-        file.write('<Resource path="' + gff3 + '"/>\n')
-    file.write('    </Resources>\n')
+        file.write('  <Resource path="../../' + gff3 + '"/>\n')
+    file.write('</Resources>\n')
 
     ## Genome annotation panel
-    file.write('    <Panel name="GeneAnnotPanel" height="60">\n')
+    file.write('<Panel name="GeneAnnotPanel" height="60">\n')
     if gff3:
-        file.write('<Track id="' + gff3 + '" name="gff3 ' + genome_version + '"  color="153,0,51" fontSize="12" >\n')
+        file.write('  <Track id="../../' + gff3 + '" name="gff3 ' + genome_version + '"  color="153,0,51" fontSize="12" >\n')
         file.write('    <DataRange/>\n')
-        file.write('</Track>\n')
+        file.write('  </Track>\n')
     if gtf:
-        file.write('<Track id="' + gtf + '" name="gtf ' + genome_version + '"  color="153,0,51" fontSize="12" >\n')
+        file.write('  <Track id="../../' + gtf + '" name="gtf ' + genome_version + '"  color="153,0,51" fontSize="12" >\n')
         file.write('    <DataRange/>\n')
-        file.write('</Track>\n')
-    file.write('    </Panel>\n')
+        file.write('  </Track>\n')
+    file.write('</Panel>\n')
 
     ## Genome coverage panel
     if coverage:
-        file.write('    <Panel name="GenomeCovPanel" height="' + str(len(coverage)*60) + '">\n')
+        file.write('<Panel name="GenomeCovPanel" height="' + str(len(coverage)*60) + '">\n')
         for bdg in coverage:
             name = bdg.split(sep="/")[-1]
-            name = name.split(sep="_")[0]
+            name = name.split(sep=".")[0]
 
-            tab = pd.read_table(bdg)
-            cov = tab.iloc[:,3]
-            max = int(np.percentile(cov, 99))
+#            tab = pd.read_table(bdg)
+#            cov = tab.iloc[:,3]
+#            max = int(np.percentile(cov, 99))
 
-            file.write('<Track height="50" autoscale="true" id="' + bdg + '" name="' + name + '" color="51,153,0" fontSize="12">\n')
+            file.write('  <Track height="50" autoscale="true" id="../../' + bdg + '" name="' + name + '" color="51,153,0" fontSize="12">\n')
             file.write('    <DataRange minimum="0" maximum="200"/>\n')
-            file.write('</Track>\n')
-        file.write('    </Panel>\n')
+            file.write('  </Track>\n')
+        file.write('</Panel>\n')
 
     ## Peaks panel
     if peaks:
-        file.write('    <Panel name="FeaturePanel" height="' + str(len(peaks)*60) + '">\n')
+        file.write('<Panel name="FeaturePanel" height="' + str(len(peaks)*60) + '">\n')
         for bedfile in peaks:
             pc_name = bedfile.split(sep="_")[-1]
             pc_name = os.path.splitext(pc_name)[0]
 
             sam_name = bdg.split(sep="/")[-1]
-            sam_name = sam_name.split(sep="_")[0]
+            sam_name = sam_name.split(sep=".")[0]
 
-            file.write('<Track id="' + bedfile + '" name="' + pc_name + " " + sam_name + '" color="0,0,178" fontSize="12">\n')
-            file.write('<DataRange/>\n')
-            file.write('</Track>\n')
-        file.write('    </Panel>\n')
+            file.write('  <Track id="../../' + bedfile + '" name="' + pc_name + " " + sam_name + '" color="0,0,178" fontSize="12">\n')
+            file.write(    '<DataRange/>\n')
+            file.write('  </Track>\n')
+        file.write('</Panel>\n')
 
-
-    file.write('    <PanelLayout/>\n')
-    file.write('    <HiddenAttributes>\n')
-    file.write('<Attribute name="NAME"/>\n')
-    file.write('<Attribute name="DATA FILE"/>\n')
-    file.write('<Attribute name="DATA TYPE"/>\n')
-    file.write('    </HiddenAttributes>\n')
+    file.write('<PanelLayout/>\n')
+    file.write('  <HiddenAttributes>\n')
+    file.write('    <Attribute name="NAME"/>\n')
+    file.write('    <Attribute name="DATA FILE"/>\n')
+    file.write('    <Attribute name="DATA TYPE"/>\n')
+    file.write('  </HiddenAttributes>\n')
     file.write('</Session>\n')
 
     file.close()
